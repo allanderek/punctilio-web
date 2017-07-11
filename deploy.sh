1ef602635b0e2be0c0743f07d33e22896d14799c
allanderek <allan.clark@gmail.com>
2017-07-11 12:27:46 +0000

    updated the deploy script to include all of the html files that are generated.

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ git checkout gh-pages &&
 git merge master &&
 python punctilio.py &&
 # TODO: check if index.html has actually changed.
-git add index.html &&
+git add index.html accreditation.html testimonials.html &&
 git commit -m "Latest build and commit to gh-pages" &&
 git push origin gh-pages &&
 git checkout master &&
