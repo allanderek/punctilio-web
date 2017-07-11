ea2d4b1fb3510e0abaea0f62bcf04756c1692b14
allanderek <allan.clark@gmail.com>
2017-07-11 12:21:07 +0000

    Add a link to the home page.

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+rm -f index.html accreditation.html testimonials.html
+git checkout gh-pages &&
+git merge master &&
+python punctilio.py &&
+# TODO: check if index.html has actually changed.
+git add index.html &&
+git commit -m "Latest build and commit to gh-pages" &&
+git push origin gh-pages &&
+git checkout master &&
+echo "Visit: https://allanderek.github.io/punctilio-web/""
